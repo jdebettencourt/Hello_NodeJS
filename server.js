f39c5b8a72ad394cc3e2aabcd073d5198861a113
James S. deBettencourt, Ph.D <james.s.debettencourt@gmail.com>
2019-06-04 17:16:21 +0000

    added gitignore

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var port = process.env.PORT;
+var port = 3000;
+var host = "0.0.0.0";
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port)
